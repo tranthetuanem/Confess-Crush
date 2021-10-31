799dd3bf07d0a67cd84ae543cbb77982a44ffaa9
tranthetuanem <93027841+tranthetuanem@users.noreply.github.com>
2021-10-31 13:11:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ const CONFIG = {
     Nắng vô tình nhưng nắng đọng trong sương
     Anh yêu em bởi vì em là nắng
     Có hạt sương nào thiếu nắng lại lung linh`,
-    btnIntro: '^^HiHi,yêu tuấn lắm cơ^^',
+    btnIntro: '^^HiHi,yêu Tuấn lắm cơ^^',
     title: 'Yêu em yêu bóng chiều tà,yêu luôn ánh mắt ngọt ngào đôi môi🥰',
     desc: 'Nói gì cho nó xa xôi,cưới anh cho khỏe đám ma cho gần 😁 ',
     btnYes: 'Phái Phái chẩy nước miếng  <33',
     btnNo: 'đéo :3',
-    question: 'Hôm nay bóng ngã chiều tà không liên quan lắm nhưng mà yêu trang làm người yêu anh nhé trang <3',
+    question: 'Hôm nay bóng ngã chiều tà không liên quan lắm nhưng mà yêu Trang! làm người yêu anh nhé Trang <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Thật ra thì em cũng yêu anh lắm i LOVE YOU TUẤN À <33333333',
     mess: 'Anh biết mà 🥰. Yêu Trang  nhiều nhiều 😘😘',
