308709ce25e5ec044312bb5b718e4357fdcac0d4
tranthetuanem <93027841+tranthetuanem@users.noreply.github.com>
2022-01-08 22:34:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tuấn Yêu Trang Nè",
-    introTitle: 'Dương Thị Huyền Trang',
+    titleWeb: "Tuấn Yêu Như Nè",
+    introTitle: 'Lê Tâm Như',
     introDesc: `Anh là sương,sương chỉ tan trong nắng
     Nắng vô tình nhưng nắng đọng trong sương
     Anh yêu em bởi vì em là nắng
@@ -10,10 +10,10 @@ const CONFIG = {
     desc: 'Nói gì cho nó xa xôi,cưới anh cho khỏe đám ma cho gần 😁 ',
     btnYes: 'Phái Phái chẩy nước miếng  <33',
     btnNo: 'đéo :3',
-    question: 'Hôm nay bóng ngã chiều tà không liên quan lắm nhưng mà yêu Trang! làm người yêu anh nhé Trang <3',
+    question: 'Hôm nay bóng ngã chiều tà không liên quan lắm nhưng mà yêu Như! làm người yêu anh nhé Trang <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Thật ra thì em cũng yêu anh lắm i LOVE YOU TUẤN À <33333333',
-    mess: 'Anh biết mà 🥰. Yêu Trang  nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu Như  nhiều nhiều 😘😘',
     messDesc: 'Vào Trong đó đi,mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/tuanusersocial.media'
